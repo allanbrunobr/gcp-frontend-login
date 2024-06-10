3465b918c73299581131999dabf97c153dd26567
Your Name <allanbruno@gmail.com>
2024-06-09 23:42:47 -0300

    Init

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,76 @@
+import { HttpClient } from '@angular/common/http';
+import {Component, inject, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import {AuthService} from "../../services/auth.service";
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, FontAwesomeModule],
+})
+export class LoginComponent implements OnInit{
+  form: FormGroup;
+  errorMessage: string | null = null;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+  onSubmit(): void {
+    const form = this.form.getRawValue();
+    this.authService
+      .login(form.email, form.password)
+      .subscribe({
+        next: () => {
+          const urlTree = this.router.createUrlTree(['/main']); // Construct UrlTree
+          this.router.navigateByUrl(urlTree);
+        },
+        error: (err) => {
+          this.errorMessage = err.code;
+        },
+      });
+  }
+
+  loginWithGoogle(): void {
+    this.authService.loginWithGoogle().subscribe({
+      next: () => {
+        const urlTree = this.router.createUrlTree(['/main']);
+        this.router.navigateByUrl(urlTree);
+      },
+      error: (err) => {
+        this.errorMessage = err.code;
+      },
+    });
+  }
+
+  loginWithGithub(): void {
+    this.authService.loginWithGithub().subscribe({
+      next: () => {
+        const urlTree = this.router.createUrlTree(['/main']);
+        this.router.navigateByUrl(urlTree);
+      },
+      error: (err) => {
+        this.errorMessage = err.code;
+      },
+    });
+  }
+
+  ngOnInit(): void {
+    const urlParams = new URLSearchParams(window.location.search);
+    const message = urlParams.get('message');
+    if (message) {
+      this.errorMessage = message;
+    }
+  }
+}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import {Router, RouterLink} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  @Input() logout!: () => void;
+
+  handleLogout(): void {
+    if (this.logout) {
+      this.logout();
+    }
+  }
+}
